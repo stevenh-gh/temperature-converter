35749c149bf91575dcbb4b740eb97480854daacf
Steven Huynh <steven.h@live.com>
2024-11-23 14:20:20 -0800

    add beforeEach to tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,12 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
+import {beforeEach} from "vitest";
+
+beforeEach(() => {
+   render(<App/>);
+});
 
 it("should have a header", () => {
-   render(<App/>);
    const header = screen.getByText("Temperature Converter");
    expect(header).toBeInTheDocument();
 });
