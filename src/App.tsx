8f3c8af19706cc6829de70fad821fcfd3dc50ac7
Steven Huynh <steven.h@live.com>
2024-11-23 13:14:52 -0800

    convert to arrow function and import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import DegreesInput from "./components/DegreesInput.tsx";
+import TempType from "./components/TempType.tsx";
+
+const App = () => {
 
    return (
       <>
          <h1>Temperature Converter</h1>
+         <DegreesInput/>
+         <TempType/>
       </>
    );
-}
+};
 
 export default App;
