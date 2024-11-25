4148751963341fb0aacb0647c7ed0ddc7ffac307
Steven Huynh <steven.h@live.com>
2024-11-24 16:06:31 -0800

    add states and passes them as props to the children components add the corresponding tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,76 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
-import {beforeEach} from "vitest";
+import {UserEvent, userEvent} from "@testing-library/user-event";
+import {beforeEach, expect} from "vitest";
+import {celsiusToFahrenheit, fahrenheitToCelsius} from "./testData.ts";
+
+let user: UserEvent;
+let degreesInput: HTMLElement;
+let tempType: HTMLElement;
+let convertBtn: HTMLElement;
 
 beforeEach(() => {
+   user = userEvent.setup();
    render(<App/>);
+
+   degreesInput = screen.getByLabelText(/degrees/i);
+   tempType = screen.getByLabelText(/type/i);
+   convertBtn = screen.getByRole("button", {name: /convert/i});
 });
 
 it("should have a header", () => {
    const header = screen.getByText("Temperature Converter");
    expect(header).toBeInTheDocument();
 });
+
+it("should render converted result when converter button is clicked (F->C)", async () => {
+   await user.type(degreesInput, "50");
+   await user.selectOptions(tempType, "fahrenheit");
+   await user.click(convertBtn);
+
+   const result = screen.getByText("10.00");
+   const resultUnit = screen.getByText("°C");
+   expect(result).toBeInTheDocument();
+   expect(resultUnit).toBeInTheDocument();
+});
+
+it("should render converted result when converter button is clicked (C->F)", async () => {
+   await user.type(degreesInput, "500");
+   await user.selectOptions(tempType, "celsius");
+   await user.click(convertBtn);
+
+   const result = screen.getByText("932.00");
+   const resultUnit = screen.getByText("°F");
+   expect(result).toBeInTheDocument();
+   expect(resultUnit).toBeInTheDocument();
+});
+
+it.each(fahrenheitToCelsius)("renders correctly for converting fahrenheit to celsius",
+   async ({
+             fahrenheit,
+             celsius
+          }) => {
+      await user.type(degreesInput, fahrenheit.toString());
+      await user.selectOptions(tempType, "fahrenheit");
+      await user.click(convertBtn);
+
+      const result = screen.getByTestId("result");
+      const resultUnit = screen.getByText("°C");
+      expect(Number(result.textContent)).toBeCloseTo(celsius, 1);
+      expect(resultUnit).toBeInTheDocument();
+   });
+
+it.each(celsiusToFahrenheit)("renders correctly for converting celsius to fahrenheit",
+   async ({
+             celsius,
+             fahrenheit
+          }) => {
+      await user.type(degreesInput, celsius.toString());
+      await user.selectOptions(tempType, "celsius");
+      await user.click(convertBtn);
+
+      const result = screen.getByTestId("result");
+      const resultUnit = screen.getByText("°F");
+      expect(Number(result.textContent)).toBeCloseTo(fahrenheit, 1);
+      expect(resultUnit).toBeInTheDocument();
+   });
--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import DegreesInput from "./components/DegreesInput.tsx";
 import TempType from "./components/TempType.tsx";
 import Convert from "./components/Convert.tsx";
 import Result from "./components/Result.tsx";
+import {useState} from "react";
+
+export interface resultPack {
+   calculatedResult: number,
+   flippedDegreeType: string
+}
 
 const App = () => {
+   const [input, setInput] = useState<number | undefined>(undefined);
+   const [temp, setTemp] = useState<string | undefined>(undefined);
+   const [result, setResult] = useState<resultPack | undefined>(undefined);
 
    return (
       <>
          <h1>Temperature Converter</h1>
-         <DegreesInput/>
-         <TempType/>
-         <Convert/>
-         <Result/>
+         <DegreesInput setInput={setInput}/>
+         <TempType setTemp={setTemp}/>
+         <Convert num={input} degType={temp} setResult={setResult}/>
+         <Result num={result?.calculatedResult} degType={result?.flippedDegreeType}/>
       </>
    );
 };
