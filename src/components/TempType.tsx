c4d48eecbb0d63855eba9fece5d703c68dd5591e
Steven Huynh <steven.h@live.com>
2024-11-24 16:04:13 -0800

    add functionality to set temperature type when option is selected

--- src/components/TempType.tsx
+++ src/components/TempType.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {SetStateAction} from "react";
 
-const TempType: React.FC = () => {
+interface TempTypeProp {
+   setTemp?: React.Dispatch<SetStateAction<string | undefined>>;
+}
+
+const TempType: React.FC<TempTypeProp> = ({setTemp}: TempTypeProp) => {
    return (
       <>
          <label htmlFor="type">Type</label>
-         <select name="type" id="type">
+         <select name="type" id="type" onChange={(e) => {
+            if (setTemp)
+               setTemp(e.target.value);
+         }}>
             <option value="fahrenheit">Fahrenheit</option>
             <option value="celsius">Celsius</option>
          </select>
