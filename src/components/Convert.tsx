cb357d96ffa2887dfa1c490d32f17ae694781f82
Steven Huynh <steven.h@live.com>
2024-11-24 16:03:06 -0800

    add props to Convert component add calculation logic when clicked

--- src/components/Convert.tsx
+++ src/components/Convert.tsx
@@ -1,7 +1,31 @@
-const Convert = () => {
+import React, {SetStateAction} from "react";
+import {resultPack} from "../App.tsx";
+
+interface ConvertProp {
+   num?: number,
+   degType?: string
+   setResult?: React.Dispatch<SetStateAction<resultPack | undefined>>
+}
+
+const Convert: React.FC<ConvertProp> = ({num, degType, setResult}: ConvertProp) => {
+   const calculate = (): void => {
+      if (setResult === undefined || num === undefined)
+         return;
+      if (degType === "fahrenheit")
+         setResult({
+            calculatedResult: (num - 32) / 1.8,
+            flippedDegreeType: "celsius"
+         });
+      else
+         setResult({
+            calculatedResult: num * 1.8 + 32,
+            flippedDegreeType: "fahrenheit"
+         });
+   };
+
    return (
       <>
-         <button>Convert</button>
+         <button onClick={calculate}>Convert</button>
       </>
    );
 };
